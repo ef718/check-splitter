a9c5a94d828859bff7ab1199b7644235daa9d730
ef718 <elfang16@gmail.com>
2015-12-06 23:09:47 -0500

    Allow for zero diners

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,9 +10,13 @@ class WelcomeController < ApplicationController
     tax = params[:tax].to_f
     tip = params[:tip].to_f
     split_tax = tax / @people
-    @split_bill = (pretax_bill / @people + split_tax)
 
+    @split_bill = (pretax_bill / @people + split_tax)
     @split_tip = (pretax_bill * (tip / 100) / @people)
+
+    @split_bill = 0 if @split_bill.nan?
+    @split_tip = 0 if @split_tip.nan?
+
     @split_total = @split_bill + @split_tip
   end
 
@@ -30,13 +34,13 @@ class WelcomeController < ApplicationController
     p5 = sum_entry(params[:p5])
 
     diner_pretax_amounts = [p1, p2, p3, p4, p5].compact
-    @total_pretax = diner_pretax_amounts.reduce(:+)
+    @total_pretax = diner_pretax_amounts.reduce(:+).to_f
 
     @group_bill = diner_pretax_amounts.map do |amount|
       {individual_bill: individual_post_tax(amount), individual_tip: individual_tip(amount), individual_total: (individual_post_tax(amount).to_f + individual_tip(amount).to_f)}
     end
 
-    @total_pretip = @total_pretax.to_f + @tax.to_f
+    @total_pretip = @total_pretax + @tax
     @total_tip = @total_pretax * (@tip_percentage / 100)
     @overall_total = @total_pretip + @total_tip
 
